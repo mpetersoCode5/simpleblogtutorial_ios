029fb75ed33d36824376ee8fdee3fbb8951ace35
Michael Peterson <michaelpeterson@Michaels-MacBook-Pro.local>
2015-01-01 11:19:25 -0500

    Started adding networking code

---
+++ simpleblogtutorial/simpleblogAPI.swift
@@ -0,0 +1,57 @@
+//
+//  simpleblogAPI.swift
+//  simpleblogtutorial
+//
+//  Created by Michael Peterson on 12/31/14.
+//  Copyright (c) 2014 Michael Peterson. All rights reserved.
+//
+
+import UIKit
+
+protocol simpleblogAPIProtocol {
+    func didReceiveResponse(results: NSDictionary)
+}
+
+class simpleblogAPI: NSObject {
+    var data : NSMutableData = NSMutableData()
+    
+    var delegate : simpleblogAPIProtocol?
+    
+    func setupConnection()
+    {
+        var urlPath = "MichaelServer/simpleblogtutorial/index.php"
+        var url : NSURL = NSURL(string: urlPath)!
+        var urlRequest : NSURLRequest = NSURLRequest(URL: url)
+        var connection : NSURLConnection = NSURLConnection(request: urlRequest, delegate: self, startImmediately: false)!
+        
+        connection.start()
+    }
+    
+    //NSURLConnection connection failed
+    func connection(connection : NSURLConnection!, didFailWithError error: NSError)
+    {
+        
+    }
+    
+    //New request so we need to clear the data object
+    func connection(didRecieveResponse: NSURLConnection!, didReceiveResponse response:NSURLResponse!)
+    {
+        self.data = NSMutableData()
+    }
+    
+    //Append incoming data
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!)
+    {
+        self.data.appendData(data);
+    }
+    
+    //NSUrlConnection Delegate Function
+    func connectionDidFinishLoading(connection : NSURLConnection!)
+    {
+        //Finished receiving data and convert it to a JSON object
+        var jsonResult : NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        
+        delegate?.didReceiveResponse(jsonResult)
+    }
+    
+}
